3451dcfb7d8becdea24c70b17d5af13de07f8bd3
Dustynus Marvell Chang <dustvc@gmail.com>
2024-06-05 15:33:09 +0700

    13: Fixed number round up

--- src/app/utils/convertToRupiah.js
+++ src/app/utils/convertToRupiah.js
@@ -2,6 +2,8 @@ export function convertToRupiah(number) {
   const formatter = new Intl.NumberFormat("id-ID", {
     style: "currency",
     currency: "IDR",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 12,
   });
   return formatter.format(number);
 }
